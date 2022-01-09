244b77cc912bd0e3b174ffc31f6cad98c525b8f4
dna2121 <dnatayawibisono@gmail.com>
2022-01-09 22:07:13 +0700

    commit

---
+++ ServerChatApp_082_DeniseNW/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using WCFService_2Way_082;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerChatApp_082_DeniseNW
+{
+    class Program
+    {
+        //
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+            try
+            {
+                hostObjek = new ServiceHost(typeof(ServiceCallback));
+                hostObjek.Open();
+
+                Console.WriteLine("Server on, ready to use ok");
+                Console.ReadLine();
+
+                hostObjek.Close();
+            }
+            catch (Exception e)
+            {
+                hostObjek = null;
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
